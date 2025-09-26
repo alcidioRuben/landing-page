59d167254500566acee00cf79bec7a691f4bb41f
Alcidio Ruben <alcidioruben525@gmail.com>
2025-09-26 18:31:27 +0200

    Alcuy1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -303,7 +303,7 @@ const Home = () => {
                 <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
                   <iframe
                     className="absolute top-0 left-0 w-full h-full rounded-xl"
-                                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&modestbranding=1&showinfo=0&controls=1&fs=1"
+                                        src="https://www.youtube.com/embed/65JGgtM0vBs"
                                         title="AMSync Ads - Como conectar seu WhatsApp"
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
